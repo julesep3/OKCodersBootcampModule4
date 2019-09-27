df8d7de19421732110dd16618bfe6fa1b0c931f0
julesep3 <50899932+julesep3@users.noreply.github.com>
2019-09-27 18:24:49 -0500

    commit

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ var express = require('express')
 var app = express()
 var port = 3000
 
+let quotes = [
+    "Fortune favors the brave.",
+    "An eye for eye only ends up making the whole world blind.",
+    "Where there is love, there is life",
+    "The best love affairs are those we never had.",
+    "Love is a better teacher than duty.",
+    "Education is what remains after one has forgotten what one has learned in school.",
+    "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe."
+]
+
 app.use('/', express.static('files'))
 
 app.get('/', function(req, res){
-    res.send("yo wassump?")
+    res.send(quotes)
 })
 
 app.listen(port, function() {
